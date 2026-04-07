a2b5a6fa4a7e55200c9518ef6771c6d72cac21da
MIAOKATZE <601586395@qq.com>
2026-04-07 14:59:30 +0800

    0.0.3-p3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,9 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+// Ensure UniMixins is available for runClient/runServer tasks even if the mod itself doesn't use mixins.
+// This fixes ClassNotFoundException for MixinTweaker in CI environments.
+dependencies {
+    runtimeOnlyNonPublishable("com.github.GTNewHorizons:UniMixins:0.4.19")
+}
